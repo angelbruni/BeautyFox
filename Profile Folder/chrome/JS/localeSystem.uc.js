4835501a8f6b036eb94f55ac1ac98d18277ab6f1
Bruno Freitas <48385307+angelbruni@users.noreply.github.com>
2023-11-19 10:42:49 +0000

    b4.3.1 - Removed separate file for checking IE Appearance.

--- Profile Folder/chrome/JS/localeSystem.uc.js
+++ Profile Folder/chrome/JS/localeSystem.uc.js
@@ -1,3 +1,51 @@
+var IsIE9PreReleaseAppearance = false;
+try {
+    IsIE9PreReleaseAppearance = Services.prefs.getBoolPref("BeautyFox.appearance.IE9PreRelease");
+    console.log("IE9 Pre-Release Appearance:", IsIE9PreReleaseAppearance);
+} catch (error) {
+    console.error("Error retrieving IE9 Pre-Release Appearance preference:", error);
+}
+
+var IsIE10DeveloperPreviewAppearance = false;
+try {
+    IsIE10DeveloperPreviewAppearance = Services.prefs.getBoolPref("BeautyFox.appearance.IE10DeveloperPreview");
+    console.log("IE10DeveloperPreview Appearance:", IsIE10DeveloperPreviewAppearance);
+} catch (error) {
+    console.error("Error retrieving IE10DeveloperPreview Appearance preference:", error);
+}
+
+var IsIE10ConsumerPreviewAppearance = false;
+try {
+    IsIE10ConsumerPreviewAppearance = Services.prefs.getBoolPref("BeautyFox.appearance.IE10ConsumerPreview");
+    console.log("IE10ConsumerPreview Appearance:", IsIE10ConsumerPreviewAppearance);
+} catch (error) {
+    console.error("Error retrieving IE10ConsumerPreview Appearance preference:", error);
+}
+
+var IsIE10ReleasePreviewAppearance = false;
+try {
+    IsIE10ReleasePreviewAppearance = Services.prefs.getBoolPref("BeautyFox.appearance.IE10ReleasePreview");
+    console.log("IE10ReleasePreview Appearance:", IsIE10ReleasePreviewAppearance);
+} catch (error) {
+    console.error("Error retrieving IE10ReleasePreview Appearance preference:", error);
+}
+
+var IsIE10Appearance = false;
+try {
+    IsIE10Appearance = Services.prefs.getBoolPref("BeautyFox.appearance.IE10");
+    console.log("IE10 Appearance:", IsIE10Appearance);
+} catch (error) {
+    console.error("Error retrieving IE10 Appearance preference:", error);
+}
+
+var IsIE11Appearance = false;
+try {
+    IsIE11Appearance = Services.prefs.getBoolPref("BeautyFox.appearance.IE11");
+    console.log("IE11 Appearance:", IsIE11Appearance);
+} catch (error) {
+    console.error("Error retrieving IE11 Appearance preference:", error);
+}
+
 var translations; // Variable to store translations
 
 // Load translations asynchronously
--- Profile Folder/chrome/windows/aboutIE/script.js
+++ Profile Folder/chrome/windows/aboutIE/script.js
@@ -1,4 +1,4 @@
-let IsIE9PreReleaseAppearance = false;
+var IsIE9PreReleaseAppearance = false;
 try {
   	IsIE9PreReleaseAppearance = Services.prefs.getBoolPref("BeautyFox.appearance.IE9PreRelease");
   	console.log("IE9 Pre-Release Appearance:", IsIE9PreReleaseAppearance);
@@ -6,7 +6,7 @@ try {
   	console.error("Error retrieving IE9 Pre-Release Appearance preference:", error);
 }
 
-let IsIE10DeveloperPreviewAppearance = false;
+var IsIE10DeveloperPreviewAppearance = false;
 try {
   	IsIE10DeveloperPreviewAppearance = Services.prefs.getBoolPref("BeautyFox.appearance.IE10DeveloperPreview");
   	console.log("IE10DeveloperPreview Appearance:", IsIE10DeveloperPreviewAppearance);
@@ -14,7 +14,7 @@ try {
   	console.error("Error retrieving IE10DeveloperPreview Appearance preference:", error);
 }
 
-let IsIE10ConsumerPreviewAppearance = false;
+var IsIE10ConsumerPreviewAppearance = false;
 try {
   	IsIE10ConsumerPreviewAppearance = Services.prefs.getBoolPref("BeautyFox.appearance.IE10ConsumerPreview");
   	console.log("IE10ConsumerPreview Appearance:", IsIE10ConsumerPreviewAppearance);
@@ -22,7 +22,7 @@ try {
   	console.error("Error retrieving IE10ConsumerPreview Appearance preference:", error);
 }
 
-let IsIE10ReleasePreviewAppearance = false;
+var IsIE10ReleasePreviewAppearance = false;
 try {
   	IsIE10ReleasePreviewAppearance = Services.prefs.getBoolPref("BeautyFox.appearance.IE10ReleasePreview");
   	console.log("IE10ReleasePreview Appearance:", IsIE10ReleasePreviewAppearance);
@@ -30,7 +30,7 @@ try {
   	console.error("Error retrieving IE10ReleasePreview Appearance preference:", error);
 }
 
-let IsIE10Appearance = false;
+var IsIE10Appearance = false;
 try {
   	IsIE10Appearance = Services.prefs.getBoolPref("BeautyFox.appearance.IE10");
   	console.log("IE10 Appearance:", IsIE10Appearance);
@@ -38,7 +38,7 @@ try {
   	console.error("Error retrieving IE10 Appearance preference:", error);
 }
 
-let IsIE11Appearance = false;
+var IsIE11Appearance = false;
 try {
   	IsIE11Appearance = Services.prefs.getBoolPref("BeautyFox.appearance.IE11");
   	console.log("IE11 Appearance:", IsIE11Appearance);
