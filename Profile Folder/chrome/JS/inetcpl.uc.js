c3fd91b5e86b4759334780d7308d3cb622df6ea1
Bruno Freitas <48385307+angelbruni@users.noreply.github.com>
2023-11-22 20:30:32 +0000

    b4.3.2 - Internet Options now uses the ShellExecuteW call from the function.

--- Profile Folder/chrome/JS/inetcpl.uc.js
+++ Profile Folder/chrome/JS/inetcpl.uc.js
@@ -11,34 +11,5 @@ function openInternetOptions() {
 }
 
 function openinetcpl() {
-    var shell32 = ctypes.open("shell32.dll");
-
-    // Define the necessary types
-    var LPCWSTR = ctypes.jschar.ptr;
-    var HINSTANCE = ctypes.voidptr_t;
-
-    // Define the function signature for ShellExecuteW
-    var ShellExecuteW = shell32.declare(
-        "ShellExecuteW",
-        ctypes.winapi_abi,
-        HINSTANCE,
-        HINSTANCE,
-        LPCWSTR,
-        LPCWSTR,
-        LPCWSTR,
-        LPCWSTR,
-        ctypes.uint32_t
-    );
-
-    // Open Internet Options
-    ShellExecuteW(
-        null,
-        "open",
-        "Rundll32.exe",
-        "shell32.dll,Control_RunDLL inetcpl.cpl",
-        null,
-        1
-    );
-
-    shell32.close();
+    runFile("Rundll32.exe", "shell32.dll,Control_RunDLL inetcpl.cpl")
 }
