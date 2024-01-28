b2fce69517a0686258295958950aa3d8bed8db48
Bruno Freitas <48385307+angelbruni@users.noreply.github.com>
2024-01-28 19:39:32 +0000

    rc3-1.0 - More Classic Theme support fixes and improvements
    - Title
    - Moved downloads.css to content folder;
    - Downloads script now adds downloads.css to DOM instead of having the stylesheet in the script.

--- Profile Folder/chrome/JS/downloads.uc.js
+++ Profile Folder/chrome/JS/downloads.uc.js
@@ -5,170 +5,18 @@
 // ==/UserScript==
 
 (function () {
-	var labelViewDownloads = getComputedStyle(document.documentElement).getPropertyValue('--label-view-downloads');
-	var labelViewNTrackYourDownloads = getComputedStyle(document.documentElement).getPropertyValue('--label-view-n-track-your-downloads');
-
 	function styleDownloads() {
 		if (!document.getElementById('downloadsListBox').getAttribute('hidden')) {
-			const places = document.getElementById("places");
-			places.setAttribute("title", labelViewDownloads + "Windows Internet Explorer");
+			var labelViewDownloads = getComputedStyle(document.documentElement).getPropertyValue('--label-view-downloads');
+			var labelViewNTrackYourDownloads = getComputedStyle(document.documentElement).getPropertyValue('--label-view-n-track-your-downloads');
 
-			var style = document.createElement('style');
-			style.innerHTML = `
-			#places {
-				color-scheme: unset !important;
-			  }
-			  #places #placesToolbox #placesToolbar {
-				padding: 16px 10px !important;
-				padding-bottom: 17px !important;
-				justify-content: end;
-				background-color: white !important;
-				border-bottom: 1px solid rgb(214, 229, 245) !important;
-				position: relative !important;
-			  }
-			  #places #placesToolbox #placesToolbar::before {
-				content: attr(data-before);
-				position: absolute;
-				left: 15px;
-				color: rgb(0, 51, 153);
-				font-size: 12pt;
-			  }
-			  #places #placesToolbox #placesToolbar > *:not(#clearDownloadsButton, #closeButton, search-textbox) {
-				display: none !important;
-			  }
-			  #places #placesToolbox #placesToolbar #clearDownloadsButton,
-			  #places #placesToolbox #placesToolbar #closeButton {
-				position: fixed;
-				bottom: 7px;
-				width: 86px !important;
-				-webkit-appearance: button !important;
-				   -moz-appearance: button !important;
-						appearance: button !important;
-				-moz-default-appearance: button !important;
-			  }
-			  #places #placesToolbox #placesToolbar #clearDownloadsButton[disabled],
-			  #places #placesToolbox #placesToolbar #closeButton[disabled] {
-				opacity: 1 !important;
-			  }
-			  #places #placesToolbox #placesToolbar #clearDownloadsButton {
-				right: 107px !important;
-			  }
-			  #places #placesToolbox #placesToolbar #closeButton {
-				right: 11px !important;
-			  }
-			  #places #placesToolbox #placesToolbar search-textbox {
-				-webkit-appearance: textfield !important;
-				   -moz-appearance: textfield !important;
-						appearance: textfield !important;
-				max-width: 252px !important;
-				min-height: 22px !important;
-				height: 22px !important;
-				position: relative !important;
-			  }
-			  #places #placesToolbox #placesToolbar search-textbox[focused] {
-				box-shadow: none !important;
-				background-color: inherit !important;
-				border-color: transparent;
-				outline: none !important;
-			  }
-			  #places #placesView #placesList {
-				min-width: 0 !important;
-				width: 0 !important;
-				max-width: 0 !important;
-			  }
-			  #places #contentView {
-				background-color: rgb(240, 240, 240) !important;
-			  }
-			  #places #contentView #placesViewsBox {
-				border-bottom: 1px solid rgb(233, 233, 233) !important;
-				margin-bottom: 39px !important;
-			  }
-			  #places #contentView #placesViewsBox richlistbox {
-				border: 0 !important;
-			  }
-			  #places #contentView #placesViewsBox richlistbox#downloadsListBox {
-				padding: 25px 9px !important;
-			  }
-			  #places #contentView #placesViewsBox richlistbox#downloadsListBox > richlistitem {
-				min-height: 77px !important;
-				align-items: center !important;
-			  }
-			  #places #contentView #placesViewsBox richlistbox#downloadsListBox > richlistitem:not([selected=true]) {
-				border-bottom: 1px rgb(220, 220, 220) solid !important;
-			  }
-			  #places #contentView #placesViewsBox richlistbox#downloadsListBox > richlistitem .downloadMainArea {
-				padding: 14px 29px !important;
-				width: calc(100% - 200px);
-			  }
-			  #places #contentView #placesViewsBox richlistbox#downloadsListBox > richlistitem .downloadMainArea .downloadTypeIcon {
-				display: none !important;
-			  }
-			  #places #contentView #placesViewsBox richlistbox#downloadsListBox > richlistitem .downloadMainArea .downloadContainer .downloadTarget {
-				font-size: 12pt !important;
-				width: 50% !important;
-			  }
-			  #places #contentView #placesViewsBox richlistbox#downloadsListBox > richlistitem .downloadButton {
-				height: 25px !important;
-				min-width: 85px !important;
-				padding: 0 !important;
-				margin-right: 33px !important;
-				color: black !important;
-				border: 1px solid white !important;
-				outline: 1px solid rgb(176, 178, 182) !important;
-				background: linear-gradient(0deg, rgb(231, 231, 231) 0%, rgb(244, 244, 244) 100%) !important;
-				justify-content: center !important;
-				border-radius: 5px !important;
-			  }
-			  #places #contentView #placesViewsBox richlistbox#downloadsListBox > richlistitem .downloadButton:hover {
-				background: linear-gradient(0deg, rgb(185, 214, 252) 0%, rgb(239, 243, 249) 100%) !important;
-			  }
-			  #places #contentView #placesViewsBox richlistbox#downloadsListBox > richlistitem .downloadButton:active, #places #contentView #placesViewsBox richlistbox#downloadsListBox > richlistitem .downloadButton[open=true] {
-				background: linear-gradient(0deg, rgb(145, 192, 253) 0%, rgb(229, 236, 246) 100%) !important;
-			  }
-			  #places #contentView #placesViewsBox richlistbox#downloadsListBox > richlistitem .downloadButton::before {
-				content: attr(tooltiptext);
-			  }
-			  #places #contentView #placesViewsBox richlistbox#downloadsListBox > richlistitem .downloadButton .button-box {
-				display: none !important;
-			  }
-			  @supports -moz-bool-pref("BeautyFox.appearance.IE10") {
-				#places #contentView #placesViewsBox richlistbox#downloadsListBox > richlistitem .downloadButton {
-				  outline: none !important;
-				  border: 1px solid rgb(172, 172, 172) !important;
-				  border-radius: 0 !important;
-				  background: linear-gradient(0deg, rgb(229, 229, 229) 0%, rgb(240, 240, 240) 100%) !important;
-				}
-				#places #contentView #placesViewsBox richlistbox#downloadsListBox > richlistitem .downloadButton:hover {
-				  border: 1px solid rgb(126, 180, 234) !important;
-				  background: linear-gradient(0deg, rgb(220, 236, 252) 0%, rgb(236, 244, 252) 100%) !important;
-				}
-				#places #contentView #placesViewsBox richlistbox#downloadsListBox > richlistitem .downloadButton:active, #places #contentView #placesViewsBox richlistbox#downloadsListBox > richlistitem .downloadButton[open=true] {
-				  border: 1px solid rgb(86, 157, 229) !important;
-				  background: linear-gradient(0deg, rgb(196, 224, 252) 0%, rgb(218, 236, 252) 100%) !important;
-				}
-			  }
-			  
-			  .textbox-search-icons {
-				display: grid !important;
-				height: 18px !important;
-				width: 18px !important;
-			  }
-			  .textbox-search-icons .textbox-search-icon {
-				list-style-image: url(chrome://userchrome/content/images/search7.svg) !important;
-				height: 16px !important;
-				width: 16px !important;
-			  }
-			  .textbox-search-icons .textbox-search-clear {
-				list-style-image: url(images/clear.svg) !important;
-				height: 18px !important;
-				width: 18px !important;
-				margin-top: -1px !important;
-			  }
-			  .textbox-search-icons .textbox-search-clear:hover {
-				list-style-image: url(images/clear-hover.svg) !important;
-			  }
-			`
+			const places = document.getElementById('places');
+			places.setAttribute('title', labelViewDownloads + 'Windows Internet Explorer');
 
+			var style = document.createElement('link');
+			style.setAttribute('rel','stylesheet');
+			style.setAttribute('href','chrome://userchrome/content/partials/content/downloads.css');
+			
 			places.appendChild(style);
 
 			const placesToolbar = document.getElementById('placesToolbar');
@@ -182,7 +30,6 @@
 
 			var clearDownloadsButton = document.getElementById('clearDownloadsButton');
 			clearDownloadsButton.setAttribute('label', 'Clear list');
-			
 		}
 	}
 
